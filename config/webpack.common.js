1cfe34463cfeee44fbea52a1c1905c499a66dd5a
Tobias Abarbanell <tobias@abarbanell.de>
2016-12-19 21:26:43 +0100

    working example tests pass.

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -1,4 +1,4 @@
-ivar webpack = require('webpack');
+var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var helpers = require('./helpers');

46ea6ba9f0dde967f198661ef44fc7a7d3cd9d59
Tobias Abarbanell <tobias@abarbanell.de>
2016-12-29 22:06:17 +0100

    code formatting

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,7 +2,9 @@ import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 describe('App', () => {
   beforeEach(() => {
-    TestBed.configureTestingModule({ declarations: [AppComponent]});
+    TestBed.configureTestingModule({ 
+        declarations: [AppComponent]
+      });
   });
   it ('should work', () => {
     let fixture = TestBed.createComponent(AppComponent);

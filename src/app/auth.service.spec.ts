a964c8638178405f842eb33ac70bccedb34c4884
Tobias Abarbanell <tobias@abarbanell.de>
2017-01-03 22:00:17 +0100

    LG-20 added test for status() return type

--- src/app/auth.service.spec.ts
+++ src/app/auth.service.spec.ts
@@ -1,8 +1,17 @@
+import { Observable } from 'rxjs/Rx';
+import { ISession } from './auth.service';
+
 // service test should be standalone without testbed
 import { AuthService } from './auth.service';
 describe('AuthService', () => {
-  it ('should work', () => {
+  it ('true should be true', () => {
     // let fixture = TestBed.createComponent(AppComponent);
     expect(true).toBe(true, 'should be true');
+  });
+  
+  it ('status() should return observable', () => {
+    let fixture = new AuthService();
+    let obs = fixture.status();
+    expect(obs instanceof Observable).toBe(true);
   });
 });
